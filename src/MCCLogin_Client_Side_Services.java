326c3afaf4e4cb34bab9f09e5c0f0c0fd0cf115c
KyletheWolff <kyle.a.wolff87@gmail.com>
2015-03-02 12:14:58 -0500

    Created the Client Service File

---
+++ src/MCCLogin_Client_Side_Services.java
@@ -0,0 +1,6 @@
+/*MCCLogin_Client_Side_Services by Kyle Wolff 3/2/2015*/
+
+
+public class MCCLogin_Client_Side_Services {
+
+}
--- src/MCCLogin_Main.java
+++ src/MCCLogin_Main.java
@@ -1,8 +1,9 @@
+/*MCCLogin_Main by Kyle Wolff 3/2/2015*/
+
 
 public class MCCLogin_Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
