8770fa3df006fd951838418278c4cbf904e6a079
KyletheWolff <kyle.a.wolff87@gmail.com>
2015-03-03 17:40:06 -0500

    Added the variables to the client service file as well as the getters and setters

--- src/MCCLogin_Client_Side_Services.java
+++ src/MCCLogin_Client_Side_Services.java
@@ -3,4 +3,39 @@
 
 public class MCCLogin_Client_Side_Services {
 
+	private String firstName; 
+	private String lastName;
+	private int studentNumber; 
+	
+	public MCCLogin_Client_Side_Services(){
+		
+		firstName = "MCC";
+		lastName = "Login";
+		studentNumber = 99999;
+	}
+	
+	public String getfirstName(){
+		
+		return firstName;
+	}
+	public void setfirstName(String First_Name){
+		
+		firstName = First_Name;
+	}
+	public String getlastName(){
+		
+		return lastName;
+	}
+	public void setlastName(String Lirst_Name){
+		
+		lastName = Lirst_Name;
+	}
+	public int getStudentNumber() {
+		
+		return studentNumber;
+	}
+	public void setStudentNumber(int Student_Number) {
+		
+		studentNumber = Student_Number;
+	}
 }
