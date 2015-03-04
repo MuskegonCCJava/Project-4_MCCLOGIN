b395ab2f7ae266afbdf956b5bbe3a70beff39db3
KyletheWolff <kyle.a.wolff87@gmail.com>
2015-03-04 01:07:00 -0500

    Added the key listeners. Will need to look more into the student number field later

--- src/MCCLogin_Main.java
+++ src/MCCLogin_Main.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.text.ParseException;
 
 import javax.swing.*;
 
@@ -8,14 +9,14 @@ import javax.swing.*;
 public class MCCLogin_Main extends JFrame {
 	// Extends JFrame is essentially making the Main file a JFrame 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		MCCLogin_Main frame = new MCCLogin_Main();
 		frame.setVisible(true);
 
 	}
 	
-	public MCCLogin_Main(){
+	public MCCLogin_Main() throws ParseException{
 		
 		MCC_Login_Panel panel = new MCC_Login_Panel(); // Created the object for the panel 
 		
@@ -32,6 +33,7 @@ public class MCCLogin_Main extends JFrame {
 		// We will place the panel in the center since we will be having a header panel 
 		// When you use add() , this is adding the panel to the frame since this is the constructor for the frame
 		
+		getRootPane().setDefaultButton(panel.submit_Button); // This will enable the Enter key to work with the button
 	}
 
 }
